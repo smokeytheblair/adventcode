9bb1cab625acfb9906886623ec0022a8ef931594
Jeff Blair <jeffreyblair@gmail.com>
2018-12-09 21:11:33 -0700

    added code to fill out the coord_map

--- advent6.py
+++ advent6.py
@@ -20,12 +20,26 @@ def load_coords(input_file):
     return coordinates
 
 
-def mark_closest_coords(coordinates):
+def mark_closest_coords(coordinates, bound, convex_hull):
     coord_map = defaultdict(dict)
 
     for index in coordinates:
         coord = coordinates[index]
-        coord_map[coord[0]][coord[1]] = 0
+        coord_map[coord[0]][coord[1]] = "{}:{}".format(index,0)
+
+        for x in range(coord[0]-1, coord[0]+1):
+            for y in range(coord[1]-1, coord[1]+1):
+                distance = abs(coord[0] - x) + abs(coord[1] - y)
+                if coord_map[x][y]:
+                    cell = coord_map[x][y]
+                    if index != int(cell.split(":")[0]):
+                        old_distsance = int(cell.split(":")[0])
+                        if distance < old_distance:
+                            coord_map[x][y] = "{}:{}".format(index, distance)
+                        elif distance == old_distance:
+                            coord_map[x][y] = "."
+                else:
+                    coord_map[x][y] = "{}:{}".format(index, distance)
 
     print(coord_map)
     return coord_map
@@ -61,9 +75,9 @@ def process_areas(input_file):
     print("Processing areas...")
 
     coordinates = load_coords(input_file)
-    coord_map = mark_closest_coords(coordinates)
     bounds = find_min_max(coordinates)
     convex_hull = find_convex_hull(coordinates)
+    coord_map = mark_closest_coords(coordinates, bounds, convex_hull)
 
 
 
