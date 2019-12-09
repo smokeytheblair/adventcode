af420ea2b38901dab2cbbe2cdb3d4a27ffdc79c8
Jeff Blair <jeffreyblair@gmail.com>
2019-12-09 13:48:03 -0700

    Day 5, part one completed.

--- 2019/advent5.py
+++ 2019/advent5.py
@@ -9,75 +9,154 @@ reset_program = None
 def print_usage(name):
     print("python3 {} <input file>".format(name))
 
+def get_param_modes(command_in):
+    command = command_in.zfill(5)
+    params_len = len(command)
+    modes = []
+    
+    if params_len == 5:
+        modes.append(int(command[0]))
+    else:
+        modes.append(0)
+
+    if params_len >= 4:
+        modes.append(int(command[1]))
+    else:
+        modes.append(0)
+
+    if params_len >= 3:
+        modes.append(int(command[2]))
+    else:
+        modes.append(0)
+
+    # print(f'modes = {modes}')
+
+    return modes
+
+def store_param(values, instr_ptr, input_param):
+    command = values[instr_ptr]
+    modes = get_param_modes(command)
+    store_idx = int(values[int(instr_ptr)+1])
+    values[store_idx] = input_param
+
+    # print(f'store_param --> {store_idx} = {input_param}')
+    
+
+def output_param(values, instr_ptr):
+    command = values[instr_ptr]
+    modes = get_param_modes(command)
+    output_idx = 0
+    if modes[2] == 0:
+       output_idx = int(values[int(instr_ptr)+1])
+       print(f'Output command = {values[output_idx]}')
+    else:
+       print(f'Output command = {values[int(instr_ptr+1)]}')
+
 def do_add(values, instr_ptr):
-    op_1_idx = values[instr_ptr+1]
-    op_2_idx = values[instr_ptr+2]
-    result_idx = values[instr_ptr+3]
-    values[result_idx] = values[op_1_idx] + values[op_2_idx]    
-    print(f'do_add at {instr_ptr} => values[{result_idx}] = {values[result_idx]}')
+    command = values[instr_ptr]
+    modes = get_param_modes(command)
+
+    op_1_idx = 0
+    op_2_idx = 0
+    result_idx = 0
+
+    if modes[2] == 0:
+        op_1_idx = int(values[instr_ptr+1])
+    else:
+        op_1_idx = instr_ptr+1
+
+    if modes[1] == 0:
+        op_2_idx = int(values[instr_ptr+2])
+    else:
+        op_2_idx = instr_ptr+2
+
+    if modes[0] == 0:
+        result_idx = int(values[instr_ptr+3])
+    else:
+        result_idx = instr_ptr+3
+
+    values[result_idx] = str(int(values[op_1_idx]) + int(values[op_2_idx]))
+    # print(f'do_add at {instr_ptr} => values[{result_idx}] = {values[result_idx]}')
 
 def do_multiply(values, instr_ptr):
-    op_1_idx = values[instr_ptr+1]
-    op_2_idx = values[instr_ptr+2]
-    result_idx = values[instr_ptr+3]
-    values[result_idx] = values[op_1_idx] * values[op_2_idx]    
-    print(f'do_multiply at {instr_ptr} => values[{result_idx}] = {values[result_idx]}')
+    command = values[instr_ptr]
+    modes = get_param_modes(command)
 
-def load_program(input_file, noun, verb):
+    op_1_idx = 0
+    op_2_idx = 0
+    result_idx = 0
+
+    if modes[2] == 0:
+        op_1_idx = int(values[instr_ptr+1])
+    else:
+        op_1_idx = instr_ptr+1
+
+    if modes[1] == 0:
+        op_2_idx = int(values[instr_ptr+2])
+    else:
+        op_2_idx = instr_ptr+2
+
+    if modes[0] == 0:
+        result_idx = int(values[instr_ptr+3])
+    else:
+        result_idx = instr_ptr+3
+
+    values[result_idx] = str(int(values[op_1_idx]) * int(values[op_2_idx]))
+    # print(f'do_multiply at {instr_ptr} => values[{result_idx}] = {values[result_idx]}')
+
+def load_program(input_file):
     global reset_program
     program = []
     if reset_program is not None:
         program = reset_program.copy()
-        program[1] = noun
-        program[2] = verb
     else:
         index = 0
 
         for line in input_file:
             values = line.split(',')
             for number in values:
-                program.append(int(number))
+                program.append(number)
                 index += 1
 
         if reset_program is None:
             reset_program = program.copy()
 
-        # repair the program
-        program[1] = noun 
-        program[2] = verb
-
     return program
 
-def process_program(input_file, noun, verb):
-    program = load_program(input_file, noun, verb)
+def process_program(input_file, input_param):
+    program = load_program(input_file)
     print(f'{program} = {len(program)}')
 
-    for instr_ptr in range(0, len(program), 4):
+    instr_ptr = 0
+
+    while  instr_ptr < len(program):
         value = program[instr_ptr]
-        if value == 99:
+        op_code = int(value[-1:])
+        # print(f'instr_ptr = {instr_ptr}, value = {value}, op_code = {op_code}')
+
+        if value == '99':
             break
-        if value is 1:
+        if op_code is 1:
             do_add(program, instr_ptr)
-        if value == 2:
+            instr_ptr += 4
+        if op_code == 2:
             do_multiply(program, instr_ptr)
+            instr_ptr += 4
+        if op_code == 3:
+            store_param(program, instr_ptr, input_param)
+            instr_ptr += 2
+        if op_code == 4:
+            output_param(program, instr_ptr)
+            instr_ptr += 2
 
-    print(f'{program} = {len(program)}')
-    print(f'program[0] = {program[0]}')
+    # print(f'{program} = {len(program)}')
+    # print(f'program[0] = {program[0]}')
     return program[0]
-
-def find_inputs(input_file, find_code):
-    for noun in range(0,100):
-        for verb in range(0,100):
-            answer = process_program(input_file, noun, verb)
-            if (answer == find_code):
-                return (noun, verb)
-
-    return
 
 def main():
     parser = argparse.ArgumentParser(description='Compute required fuel for modules, or modules+fuel')
     parser.add_argument('file', type=argparse.FileType('r'))
-    parser.add_argument('--find-code', type=int, required=False, default=0, help='Compute fuel for modules plus the loaded fuel.')
+    parser.add_argument('--input', type=int, required=False, default=0, help='Compute fuel for modules plus the loaded fuel.')
 
     args = parser.parse_args()
 
@@ -85,13 +164,13 @@ def main():
 
     if (len(sys.argv) > 1):
         with args.file as input_file:
-            if args.find_code is 0:
-                answer = process_program(input_file, 12, 2)
+            if args.input is 1:
+                answer = process_program(input_file, int(args.input))
                 print(f'Answer = {answer}')
-            else:
-                noun, verb = find_inputs(input_file, args.find_code)
-                answer = 100 * noun + verb
-                print(f'noun = {noun}, verb = {verb}, answer = {answer}')
+            # else:
+            #     noun, verb = find_inputs(input_file, args.input)
+            #    answer = 100 * noun + verb
+            #    print(f'noun = {noun}, verb = {verb}, answer = {answer}')
     else:
         print_usage(sys.argv[0])
 
