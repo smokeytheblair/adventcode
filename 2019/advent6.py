436d2130eed4237c555b553f2b1da5e0625ae924
Jeff Blair <jeffreyblair@gmail.com>
2019-12-10 16:54:32 -0700

    Day 6, part 1 done.

--- 2019/advent6.py
+++ 2019/advent6.py
@@ -1,27 +1,54 @@
 #!/usr/bin/env python3
 
 import sys
-import math
 import argparse
+from collections import defaultdict
+
+def find_path(graph, start, end, path=None):
+    if path is None:
+        path = []
+
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
 
 def load_program(orbit_map):
-    pass
+    orbits = defaultdict(list)
+    planets = set()
+
+    for orbit in orbit_map:
+        parent_node, child_node = orbit.split(')')
+        planets.add(parent_node)
+        planets.add(child_node[:-1])
+        orbits[parent_node].append(child_node[:-1])
+
+    # print(f'planets = {planets}')
+    return (orbits, planets)
 
 def process_program(orbit_map):
-    orbits = load_program(orbit_map)
-    print(f'{orbits}')
+    orbits, planets = load_program(orbit_map)
+#    for orbit in orbits:
+#        print(f'{orbit}: {orbits[orbit]}')
     orbit_count = 0
 
+    for planet1 in planets:
+        for planet2 in planets:
+            path = find_path(orbits, planet1, planet2, []) 
+            if path is not None and 1 < len(path):
+                orbit_count += 1
+#                if len(path) > 0:
+#                print(f'path({planet1}, {planet2}) --> {path}: orbit_count = {orbit_count}')
+
+    print(f'orbit_count = {orbit_count}')
+
     return orbit_count
-
-def find_inputs(input_file, find_code):
-    for noun in range(0,100):
-        for verb in range(0,100):
-            answer = process_program(input_file, noun, verb)
-            if (answer == find_code):
-                return (noun, verb)
-
-    return
 
 def main():
     parser = argparse.ArgumentParser(description='Compute direct + indirect orbits in the orbit map.')
@@ -38,9 +65,10 @@ def main():
                 answer = process_program(input_file)
                 print(f'Answer = {answer}')
             else:
-                noun, verb = find_inputs(input_file, args.find_code)
-                answer = 100 * noun + verb
-                print(f'noun = {noun}, verb = {verb}, answer = {answer}')
+                pass
+#                noun, verb = find_inputs(input_file, args.find_code)
+#                answer = 100 * noun + verb
+#                print(f'noun = {noun}, verb = {verb}, answer = {answer}')
     else:
         print_usage(sys.argv[0])
 
