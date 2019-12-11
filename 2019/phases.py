0a8e582b787e17182b5fd12433d6a4817e05b33b
Jeff Blair <jeffreyblair@gmail.com>
2019-12-11 15:32:02 -0700

    Day 7 part 2, phases file

--- 2019/phases.py
+++ 2019/phases.py
@@ -9,13 +9,13 @@ def unique(list1):
             unique_list.append(x)
     return unique_list
 
-for a in range(5):
-    for b in range(5):
-        for c in range(5):
-            for d in range(5):
-                for e in range(5):
+for a in range(5,10):
+    for b in range(5,10):
+        for c in range(5,10):
+            for d in range(5,10):
+                for e in range(5,10):
                     perms = [a, b, c, d, e]
                     perms = unique(perms)
 
                     if len(perms) == 5:
-                        print(f'{perms}')
+                        print(f'{perms[0]},{perms[1]},{perms[2]},{perms[3]},{perms[4]}')
