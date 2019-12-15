2e61427e868236e93dc772ceed45150c52f4de31
Jeff Blair <jeffreyblair@gmail.com>
2019-12-14 23:45:24 -0700

    Day 8 part 2, done. collapse image layers and read message.

--- 2019/advent8.py
+++ 2019/advent8.py
@@ -4,9 +4,29 @@ import sys
 import math
 import argparse
 from collections import defaultdict
+import numpy as np
 
 def print_usage(name):
     print("python3 {} <input file>".format(name))
+
+def print_image(collapsed_image,dim_x, dim_y):
+    for row in range(dim_y):
+        print(f'{collapsed_image[dim_x*row:dim_x+(dim_x*row)]}')
+
+def collapse_image_layers(image_file, dim_x, dim_y):
+    pixels_per_layer = dim_x * dim_y
+    collapsed_layer = np.full(pixels_per_layer, 2)
+
+    for line in image_file:
+        image = line.strip()
+        for index in range(len(image)):
+            pixel = int(image[index])
+            layer_index = index % pixels_per_layer
+            if pixel != 2 and collapsed_layer[layer_index] == 2:
+                collapsed_layer[layer_index] = pixel
+
+    print(f'collapsed_layer = {collapsed_layer}, len: {len(collapsed_layer)}')
+    return collapsed_layer
 
 def compute_image_crc(image_file, dim_x, dim_y):
     current_layer = 1
@@ -48,7 +68,7 @@ def main():
     parser.add_argument('file', type=argparse.FileType('r'))
     parser.add_argument('--x-dim', type=int)
     parser.add_argument('--y-dim', type=int)
-    parser.add_argument('--modules-and-fuel', action='store_true', default=False, help='Compute fuel for modules plus the loaded fuel.')
+    parser.add_argument('--print', action='store_true', default=False, help='Compute fuel for modules plus the loaded fuel.')
 
     args = parser.parse_args()
 
@@ -56,8 +76,12 @@ def main():
 
     if (len(sys.argv) > 1):
         with args.file as input_file:
-            image_crc = compute_image_crc(input_file, args.x_dim, args.y_dim)
-            print(f'Min layer CRC = {image_crc}')
+            if args.print is True:
+                collapsed_image = collapse_image_layers(input_file, args.x_dim, args.y_dim)
+                print_image(collapsed_image, args.x_dim, args.y_dim)
+            else:
+                image_crc = compute_image_crc(input_file, args.x_dim, args.y_dim)
+                print(f'Image hash = {image_crc}')
     else:
         print_usage(sys.argv[0])
 
