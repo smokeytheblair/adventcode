e017af1680a76154c4f1b2e1b5b9efcb51a5afad
Jeff Blair <jeffreyblair@gmail.com>
2020-11-18 21:32:31 -0700

    boilerplate main and file io

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,1 +1,22 @@
+import sys
+import math
+import argparse
+
+def print_usage(name, input_file):
+    print(f"Usage: python3 {name} {input_file}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Compute required fuel for modules, or modules+fuel')
+    parser.add_argument('file', type=argparse.FileType('r'))
+    parser.add_argument('--modules-and-fuel', action='store_true', default=False, help='Compute fuel for modules plus the loaded fuel.')
+
+    args = parser.parse_args()
+
+    if (len(sys.argv) > 1):
+        pass
+    else:
+        print_usage(sys.argv[0], args.file)
+
+if __name__ == "__main__":
+    main()
 
