10288da3b3f043be983025f43c8d43b107a48e95
Jeff Blair <jeffreyblair@gmail.com>
2020-12-01 10:30:56 -0700

    pass in number 2/3 to check

--- 2020/day1.py
+++ 2020/day1.py
@@ -21,31 +21,39 @@ def load_expense_report(input_file):
 
     return report
      
-def process_expense_report(input_file):
+def process_expense_report(input_file, number):
     report = load_expense_report(input_file)
 
-    for num_a in report:
-#        print(f"from report: num_a = {num_a}")
-        for num_b in report:
-            for num_c in report:
+    if number == 2:
+        for num_a in report: 
+            for num_b in report: 
                 num_1 = int(num_a)
                 num_2 = int(num_b)
-                num_3 = int(num_c)
-#            print(f"{num_1} + {num_2} == {num_1 + num_2}\n{num_1} * {num_2} == {num_1 * num_2}")
-                if num_1 + num_2 + num_3 == 2020:
-                    print(f"{num_1} + {num_2} + {num_3} == {num_1+num_2+num_3}\n{num_1} * {num_2} * {num_3} == {num_1 * num_2 * num_3}")
+                if num_1 + num_2 == 2020:
+                    print(f"{num_1} + {num_2} == {num_1+num_2}\n{num_1} * {num_2} == {num_1 * num_2}")
                     break
+
+    if number == 3:
+        for num_a in report:
+            for num_b in report:
+                for num_c in report:
+                    num_1 = int(num_a)
+                    num_2 = int(num_b)
+                    num_3 = int(num_c)
+                    if num_1 + num_2 + num_3 == 2020:
+                        print(f"{num_1} + {num_2} + {num_3} == {num_1+num_2+num_3}\n{num_1} * {num_2} * {num_3} == {num_1 * num_2 * num_3}")
+                        break
 
 def main():
     parser = argparse.ArgumentParser(description='Compute required fuel for modules, or modules+fuel')
     parser.add_argument('file', type=argparse.FileType('r'))
-#    parser.add_argument('--modules-and-fuel', action='store_true', default=False, help='Compute fuel for modules plus the loaded fuel.')
+    parser.add_argument('--number', type=int, required=True, help='How many nums to add/multiply.')
 
     args = parser.parse_args()
 
     if (len(sys.argv) > 1):
         with args.file as input_file:
-            process_expense_report(input_file)
+            process_expense_report(input_file, args.number)
     else:
         print_usage(sys.argv[0], args.file)
 
