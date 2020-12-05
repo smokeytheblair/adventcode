5059ec7d415caf053f62e7444450931ca90d2914
Jeff Blair <jeffreyblair@gmail.com>
2020-12-04 23:53:09 -0700

    day 5 part 2 still wrong

--- 2020/day5.py
+++ 2020/day5.py
@@ -57,26 +57,26 @@ def find_column(boarding_pass, min_col, max_col):
         
     return col
 
-def seat_id(boarding_pass):
+def seat_id(boarding_pass, min_row, max_row, min_col, max_col):
     current_seat_id = 0
 
-    row = find_row(boarding_pass, 0, 127)
+    row = find_row(boarding_pass, min_row, max_row-1)
 #    print(f"{boarding_pass} row {row}")
-    col = find_column(boarding_pass, 0, 7)
+    col = find_column(boarding_pass, min_col, max_col-1)
 #    print(f"{boarding_pass} col {col}")
 
     current_seat_id = (row * 8) + col
 
     return current_seat_id
 
-def part_1(input_file):
+def part_1(input_file, min_row, max_row, min_col, max_col):
     passes = load_inputs(input_file)
     
-    print(passes)
+#    print(passes)
     highest_seat = 0
 
     for boardingpass in passes:
-        current_seat_id = seat_id(boardingpass)
+        current_seat_id = seat_id(boardingpass, min_row, max_row, min_col, max_col)
         print(f"{boardingpass} has seat id {current_seat_id}")
         if highest_seat < current_seat_id:
             highest_seat = current_seat_id
@@ -84,8 +84,33 @@ def part_1(input_file):
     print(f"Highest seat id: {highest_seat}")
     return highest_seat
 
-def part_2(input_file):
-    pass
+def part_2(input_file, min_row, max_row, min_col, max_col):
+    passes = load_inputs(input_file)
+    
+#    print(passes)
+    my_seat = 0
+
+    seats = []
+    for boardingpass in passes:
+        current_seat_id = seat_id(boardingpass, min_row, max_row, min_col, max_col)
+#        print(f"{boardingpass} has seat id {current_seat_id}")
+        if current_seat_id not in seats:
+            seats.append(current_seat_id)
+
+    last_seat = 0
+    seats.sort()
+    print(seats)
+    for sid in seats:
+        if last_seat == 0:
+            last_seat = sid
+
+        if sid != last_seat and abs(sid - last_seat) == 2:
+            my_seat = last_seat + 1
+
+        
+    print(f"My seat id: {my_seat}")
+    return my_seat
+
 
 def main():
     parser = argparse.ArgumentParser(description="compute boarding passes.")
@@ -97,9 +122,9 @@ def main():
     if (len(sys.argv) > 1):
         with args.file as input_file:
             if args.part == 1:
-                part_1(input_file)
+                part_1(input_file, 0, 127, 0, 7)
             elif args.part == 2:
-                part_2(input_file)
+                part_2(input_file, 0, 127, 0, 7)
     else:
         print_usage(sys.argv[0], args.file)
 
