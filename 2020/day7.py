dd9a62203ad944ae771dc3ca5319ce8ba89ecc5c
Jeff Blair <jeffreyblair@gmail.com>
2020-12-06 23:52:41 -0700

    day 7 part 1 completed

--- 2020/day7.py
+++ 2020/day7.py
@@ -17,21 +17,68 @@ def load_inputs(input_file):
         report = reset_report.copy()
     else:
         for line in input_file:
-            report.append(line[:-1])
+            report.append(line[:-2]) #remove \n and .
 
         if reset_report is None:
             reset_report = report.copy()
 
     return report
 
+def make_rule_tree(rules):
+    rule_tree = dict()
+
+    for rule in rules:
+        splits = rule.split(" contain ")
+#        print(f"{splits}")
+        key = splits[0]
+        if "," in splits[1]:
+            value = splits[1].split(", ")
+        else:
+            value = [splits[1]]
+
+        key = key.replace(" bags", "")
+        key = key.replace(" bag", "")
+
+        new_vals = []
+        for val in value:
+            val = val.replace(" bags", "")
+            val = val.replace(" bag", "")
+            new_vals.append(val)
+
+        rule_tree[key] = new_vals
+
+    return rule_tree
+
+def find_gold_bag(rule_tree, current_rule):
+    if rule_tree.get(current_rule) is None:
+        return False
+
+    for inner_rule in rule_tree[current_rule]:
+        if "shiny gold" in inner_rule:
+            return True
+
+        if find_gold_bag(rule_tree, inner_rule[2:]):
+            return True
+
+    return False
+
 def part_1(input_file):
     rules = load_inputs(input_file)
+    rules = make_rule_tree(rules)
     
 #    print(rules)
 
-    counts = []
-    for rule in rules:
-        print(rule)
+    count = 0
+    for rule in rules.keys():
+#        perint(rule)
+#        for inner in rules[rule]:
+#            print(f"\t- {inner}")
+
+        if find_gold_bag(rules, rule):
+            count += 1
+            print(f"{rule} can hold shiny gold")
+
+    print(f"Bags that can hold shiny gold: {count}")
 
 def part_2(input_file):
     pass
