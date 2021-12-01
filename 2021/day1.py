c89ae2995db53c22669eee843b9c550650424866
Jeff Blair <jeffreyblair@gmail.com>
2021-12-01 12:41:22 -0700

    Day 1 part 1 completed answer: 1553

---
+++ 2021/day1.py
@@ -0,0 +1,58 @@
+import sys
+import argparse
+import math
+from collections import Counter
+from operator import xor
+
+reset_report = None
+
+def print_usage(name, input_file):
+    print(f"Usage: python3 {name} {input_file}")
+
+def load_inputs(input_file):
+    global reset_report
+    report = []
+
+    if reset_report is not None:
+        report = reset_report.copy()
+    else:
+        report = list(input_file)
+
+        if reset_report is None:
+            reset_report = report.copy()
+
+    return report
+
+def count_depths(input_file):
+    depths = load_inputs(input_file)
+
+    count = 0
+    prev_depth = 0
+    for depth in depths:
+        if 0 != prev_depth and int(depth) > prev_depth:
+            count += 1
+
+        prev_depth = int(depth)
+
+    print(f"Count = {count}")
+     
+def main():
+    parser = argparse.ArgumentParser(description="Count depth increases.")
+    parser.add_argument('file', type=argparse.FileType('r'))
+    parser.add_argument('--part', type=int, required=True, help='Part 1 or Part 2')
+
+    args = parser.parse_args()
+
+    if (len(sys.argv) > 1):
+        with args.file as input_file:
+            if args.part == 1:
+                count_depths(input_file)
+            elif args.paert == 2:
+                with args.slopes as slopes_file:
+                    count_trees_2(input_file, slopes_file)
+    else:
+        print_usage(sys.argv[0], args.file)
+
+if __name__ == "__main__":
+    main()
+
