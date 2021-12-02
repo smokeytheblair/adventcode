163445ac6d8dcc79d1cc1f0e108ea4660b0e3977
Jeff Blair <jeffreyblair@gmail.com>
2021-12-02 10:54:13 -0700

    day2 part 1 complete. Answer: 2150351

---
+++ 2021/day2.py
@@ -0,0 +1,79 @@
+import sys
+import argparse
+
+reset_report = None
+
+def print_usage(name, input_file):
+    print(f"Usage: python3 {name} {input_file}")
+
+def load_inputs(input_file):
+    global reset_report
+    report = []
+
+    if reset_report is not None:
+        report = reset_report.copy()
+    else:
+        report = list(input_file)
+
+        if reset_report is None:
+            reset_report = report.copy()
+
+    tuples = []
+    for line in report:
+        direction = line.split()
+#        print(f"line: {line}, direction: {direction}")
+        tuples.append((direction[0], int(direction[1])))
+
+    return tuples
+
+def compute_path(input_file):
+    directions = load_inputs(input_file)
+#    print(f"{directions}")
+
+    forward = 0
+    depth = 0
+    for direction in directions:
+        if direction[0] == "forward":
+            forward += direction[1]
+        elif direction[0] == "up":
+            depth -= direction[1]
+        elif direction[0] == "down":
+            depth += direction[1]
+
+    print(f"forward:{forward} * depth:{depth} = {forward*depth}")
+
+    
+def count_depths_2(input_file):
+    depths = load_inputs(input_file)
+
+    count = 0
+    for index in range(len(depths)):
+        if index < len(depths)-3:
+            A = int(depths[index]) + int(depths[index+1]) + int(depths[index+2])
+            B = int(depths[index+1]) + int(depths[index+2]) + int(depths[index+3])
+#            print(f"A = {A} < B = {B}")
+            if A < B:
+                count += 1
+#                print(f"count = {count}")
+
+    print(f"Count = {count}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Compute path.")
+    parser.add_argument('file', type=argparse.FileType('r'))
+    parser.add_argument('--part', type=int, required=True, help='Part 1 or Part 2')
+
+    args = parser.parse_args()
+
+    if (len(sys.argv) > 1):
+        with args.file as input_file:
+            if args.part == 1:
+                compute_path(input_file)
+            elif args.part == 2:
+                count_depths_2(input_file)
+    else:
+        print_usage(sys.argv[0], args.file)
+
+if __name__ == "__main__":
+    main()
+
