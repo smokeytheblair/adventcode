112d1569cec9d1489cd6e1177af409ee20fe646e
Jeff Blair <jeffreyblair@gmail.com>
2021-12-06 13:55:06 -0700

    day 4 part 1 done. Answer: 41668

--- 2021/day4.py
+++ 2021/day4.py
@@ -7,6 +7,17 @@ bingo_cards = None
 def print_usage(name, input_file):
     print(f"Usage: python3 {name} {input_file}")
 
+def print_card_sel(card_num, x, y):
+    print(f"bingo_cards card {card_num} [{x},{y}] = {bingo_cards[card_num][x][y]}")
+
+def print_bingo_cards():
+    index = 0
+    for card in bingo_cards:
+        print(f"Card {index}:")
+        for row in card:
+            print(row)
+        index += 1
+
 def load_inputs(input_file):
     global bingo_balls
     global bingo_cards
@@ -16,69 +27,96 @@ def load_inputs(input_file):
     bingo_balls = lines[0].replace("\n", "").split(",")
     print(bingo_balls)
 
+    bingo_cards = []
     card = []
     next_card = False
     for line in lines:
         line = line.replace("\n", "")
         if -1 < line.find(","):
             continue
-        if line == "":
+        if line == "" or line == "\n":
             if 0 != len(card):
-                print(card)
+                print(f"1: {card}")
                 bingo_cards.append(card)
             card = list()
+            print(f"2: {card}")
         else:
             row = line.split(" ")
-            print(row)
-            if 0 == len(card):
-                for num in row:
-                    card.append(list(num))
-            else:
-                index = 0
-                for num in row:
-                    card[index].append(num)
-                    index += 1
+            print(f"line: {line}, row: {row}")
+            card.append([[num, False] for num in row if num != ""])
 
+    if 0 != len(card):
+        bingo_cards.append(card)
 
-    print(cards)
-    # for line in report:
+    print_bingo_cards()
 
-    return report
+    return bingo_cards
+
+def play_ball(ball_num):
+    print(f"Playing number {ball_num}")
+    for card in bingo_cards:
+        for row in card:
+            for num in row:
+                if num[0] == ball_num:
+                    num[1] = True
+
+    print_bingo_cards()
+
+def card_wins(card):
+    #check rows
+    for row in card:
+        row_wins = True
+        for num in row:
+            if num[1] == False:
+                row_wins = False
+        if True == row_wins:
+           return True
+
+    #check columns
+    col_wins = [True] * len(card[0])
+    for row in card:
+        col_id = 0
+        for num in row:
+            if num[1] == False:
+                col_wins[col_id] = False
+            col_id += 1
+
+    if True in col_wins:
+        return True
+
+    return False
+
+def check_cards():
+    index = 0
+    for card in bingo_cards:
+        if card_wins(card):
+            return index
+        index += 1
+
+    return -1
+
+def sum_unmarked_numbers(card):
+    sum = 0
+    for row in card:
+        for num in row:
+            if num[1] == False:
+                sum += int(num[0])
+
+    return sum
 
 def play_bingo(input_file):
-    directions = load_inputs(input_file)
-#    print(f"{directions}")
+    load_inputs(input_file)
+    
+    for num in bingo_balls:
+        play_ball(num)
 
-    num_bits = len(directions[0])
-    count_0 = 0
-    count_1 = 0
-    gamma_rate = ""
-    epsilon_rate = ""
-    for i in range(num_bits):
-        count_0 = 0
-        count_1 = 0
- #       print(f"i: {i}")
-        for direction in directions:
-  #          print(f"direction[{i}] = {direction[i]}")
-            if direction[i] == "0":
-                count_0 += 1
-            else:
-                count_1 += 1
+        winner = check_cards()
 
-#        print(f"count_0:{count_0}, count_1:{count_1}")
-
-        if count_0 > count_1:
-            gamma_rate += "0"
-            epsilon_rate += "1"
-        else:
-            gamma_rate += "1"
-            epsilon_rate += "0"
-
-
-    gamma_int = int(gamma_rate, 2)
-    epsilon_int = int(epsilon_rate, 2)
-
-    print(f"gamma rate:{gamma_int} * epsilon rate:{epsilon_int} = {gamma_int*epsilon_int}")
+        if -1 < winner:
+            print(f"Winning card {winner}")
+            unmarked_sum = sum_unmarked_numbers(bingo_cards[winner])
+            print(f"Answer: {unmarked_sum} * {num} = {unmarked_sum * int(num)}")
+            break
 
 def filter_o2_numbers(codes, i):
 #    print(f"filter o2 i: {i}, codes: {codes}")
