78632bf86c3387bf960b311ca4027311d4747360
Jeff Blair <jeffreyblair@gmail.com>
2021-12-14 11:27:27 -0700

    day 5 still in progress.  working on test data, but not on main data.

--- 2021/day5.py
+++ 2021/day5.py
@@ -19,13 +19,138 @@ def load_inputs(input_file):
 
     return vectors
 
-def plot_vectors():
-    pass
+def count_overlaps(vectors):
+    count = 0
+    already_checked = {}
+    for vector_1 in vectors:
+        for vector_2 in vectors:
+            if vector_1 is vector_2:
+                continue
+
+            # if None is not already_checked.get(str(vector_1)):
+            #     continue
+
+            if None is not already_checked.get(str(vector_2)):
+                continue
+
+            #keep the lines going left to right
+            if int(vector_1[1][0]) < int(vector_1[0][0]):
+                x_0 = int(vector_1[1][0])
+                y_0 = int(vector_1[1][1])
+                x_1 = int(vector_1[0][0])
+                y_1 = int(vector_1[0][1])
+                #print(f"swapped {vector_1}: p0 [{x_0},{y_0}] and p1 [{x_1},{y_1}] to keep line left to right")
+            elif int(vector_1[1][0]) == int(vector_1[0][0]) and int(vector_1[0][1]) > int(vector_1[1][1]):
+                x_0 = int(vector_1[1][0])
+                y_0 = int(vector_1[1][1])
+                x_1 = int(vector_1[0][0])
+                y_1 = int(vector_1[0][1])
+                #print(f"swapped {vector_1}: p0 [{x_0},{y_0}] and p1 [{x_1},{y_1}] to keep line top down")
+            else:
+                x_0 = int(vector_1[0][0])
+                y_0 = int(vector_1[0][1])
+                x_1 = int(vector_1[1][0])
+                y_1 = int(vector_1[1][1])
+
+            #skip diagonal lines
+            x_s_1 = x_1 - x_0
+            y_s_1 = y_1 - y_0
+            if 0 != x_s_1 and 0 != y_s_1:
+                # print(f"Slope of x1: {x_1} - {x_0} = {x_s_1}")
+                # print(f"Slope of y1: {y_1} - {y_0} = {y_s_1}")
+                continue
+
+            #keep the lines going left to right
+            if int(vector_2[1][0]) < int(vector_2[0][0]):
+                x_2 = int(vector_2[1][0])
+                y_2 = int(vector_2[1][1])
+                x_3 = int(vector_2[0][0])
+                y_3 = int(vector_2[0][1])
+                #print(f"swapped {vector_2}: p2 [{x_2},{y_2}] and p3 [{x_3},{y_3}] to keep line left to right")
+            elif int(vector_2[1][0]) == int(vector_2[0][0]) and int(vector_2[0][1]) > int(vector_2[1][1]):
+                x_0 = int(vector_2[1][0])
+                y_0 = int(vector_2[1][1])
+                x_1 = int(vector_2[0][0])
+                y_1 = int(vector_2[0][1])
+                #print(f"swapped {vector_2}: p2 [{x_2},{y_2}] and p3 [{x_3},{y_3}] to keep line top down")
+            else:
+                x_2 = int(vector_2[0][0])
+                y_2 = int(vector_2[0][1])
+                x_3 = int(vector_2[1][0])
+                y_3 = int(vector_2[1][1])
+
+            #skip diagonal lines
+            x_s_2 = x_3 - x_2
+            y_s_2 = y_3 - y_2
+            if 0 != x_s_2 and 0 != y_s_2:
+                # print(f"Slope of x2: {x_3} - {x_2} = {x_s_2}")
+                # print(f"Slope of y2: {y_3} - {y_2} = {y_s_2}")
+                continue
+
+            #make left most vector the first vector
+            if (x_2 < x_0 and y_0 == y_2) or (x_0 == x_2 and y_2 < y_0):
+                temp_x = x_0
+                x_0 = x_2
+                x_2 = temp_x
+                temp_y = y_0
+                y_0 = y_2
+                y_2 = temp_y
+
+                temp_x = x_1
+                x_1 = x_3
+                x_3 = temp_x
+                temp_y = y_1
+                y_1 = y_3
+                y_3 = temp_y
+
+            print(f"comparing vector_1:{vector_1} to vector_2:{vector_2} as \n*** p0[{x_0},{y_0}], p1[{x_1},{y_1}], p2[{x_2},{y_2}], p3[{x_3},{y_3}]")
+
+            if y_0 == y_1 == y_2 == y_3:
+                if x_1 > x_2:
+                    count_delta = x_1 - x_2
+                    if x_3 < x_1:
+                        count_delta -= x_3
+
+                    print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : {count_delta} (parallel)")
+                    count += count_delta
+                elif x_1 == x_2:
+                    print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : 1 (parallel)")
+                    count += 1
+            elif x_0 == x_1 == x_2 == x_3:
+                if y_1 > y_2:
+                    count_delta = y_1 - y_2
+                    if y_3 < y_1:
+                        count_delta -= y_3
+                    print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : {count_delta} (parallel)")
+                    count += count_delta
+                elif y_1 < y_2:
+                    count_delta = y_2 - y_1
+                    if y_3 > y_1:
+                        count_delta -= y_1
+                    print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : {count_delta} (parallel)")
+                    count += count_delta
+                elif y_1 == y_2:
+                    print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : 1 (parallel)")
+                    count += 1
+            elif y_0 == y_1 and x_0 <= x_2 and x_3 <= x_1 and y_0 >= y_2 and y_1 <= y_3:
+                print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : 1")
+                count += 1
+            elif x_0 == x_1 and x_0 <= x_2 and x_3 >= x_1 and y_0 <= y_2 and y_1 >= y_3:
+                print(f"vector_1: {vector_1} intersects with vector_2: {vector_2} : 1")
+                count += 1
+
+            already_checked[str(vector_1)] = str(vector_2)
+            #already_checked[str(vector_2)] = str(vector_1)
+
+    return count
 
 def find_overlaps(input_file):
     vectors = load_inputs(input_file)
-
     print(vectors)
+
+    count = count_overlaps(vectors)
+
+    print(f"Count overlaps: {count}")
 
 def main():
     parser = argparse.ArgumentParser(description="Vents")
