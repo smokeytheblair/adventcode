bc5d15c3f2aaa0b3567ad985f9831ffd5f3bf348
Jeff Blair <jeffreyblair@gmail.com>
2022-12-07 10:21:10 -0700

    boiler plate that reads in the input file

---
+++ 2022/boiler_plate.py
@@ -0,0 +1,49 @@
+import sys
+import argparse
+
+reset_report = None
+
+
+def load_inputs(input_file):
+    global reset_report
+    report = []
+
+    if reset_report is not None:
+        report = reset_report.copy()
+    else:
+        report = list(input_file)
+
+        if reset_report is None:
+            reset_report = report.copy()
+
+    return report
+
+
+def count_calories(input_file):
+    inputs = load_inputs(input_file)
+
+
+def count_calories_2(input_file):
+    inputs = load_inputs(input_file)
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Count depth increases.")
+    parser.add_argument('file', type=argparse.FileType('r'))
+    parser.add_argument('--part', type=int, required=True, help='Part 1 or Part 2')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) > 1:
+        with args.file as input_file:
+            if args.part == 1:
+                count_calories(input_file)
+            elif args.part == 2:
+                count_calories_2(input_file)
+
+
+
+if __name__ == "__main__":
+    main()
+
