4074af7c332adae41f3f0573f7db4789b67a84d6
Jeff Blair <jeffreyblair@gmail.com>
2023-12-02 12:15:21 -0700

    day2 part2 working: 66016

--- 2023/day2.py
+++ 2023/day2.py
@@ -34,28 +34,58 @@ def parse_game(game_report:str, loaded_cubes):
             print(color_count)
 
             if int(color_count[0].strip()) > loaded_cubes[color_count[1]]:
-                return ( False, game_id )
+                return False, game_id
 
-    return (True, game_id)
+    return True, game_id
 
 
 def part1(input_file):
     inputs = load_inputs(input_file)
 
-    possible_games = []
     loaded_cubes = {'red': 12, 'green': 13, 'blue': 14}
 
     possible_draws_sum = 0
     for game_report in inputs:
         game = parse_game(game_report, loaded_cubes)
 
-        if game[0] == True:
+        if game[0]:
             possible_draws_sum += game[1]
 
     print(f'Possible draws sum: {possible_draws_sum}')
 
+
+def parse_game2(game_report:str):
+    game_id = int(game_report[5:game_report.index(':')])
+
+    draws = game_report[game_report.index(':')+1:].split(';')
+    print(draws)
+
+    min_cubes = {'red': 0, 'green': 0, 'blue': 0}
+
+    for draw in draws:
+        print(draw)
+
+        cubes_by_color = draw.strip().split(',')
+        for color in cubes_by_color:
+            color_count = color.strip().split(' ')
+            print(color_count)
+
+            current_count = int(color_count[0].strip())
+            if current_count > min_cubes[color_count[1]]:
+                min_cubes[color_count[1]] = int(color_count[0].strip())
+
+    return game_id, min_cubes
+
+
 def part2(input_file):
     inputs = load_inputs(input_file)
+
+    sum_of_mins = 0
+    for game_report in inputs:
+        game = parse_game2(game_report)
+        sum_of_mins += game[1]['red'] * game[1]['green'] * game[1]['blue']
+
+    print(f'Sum of mins: {sum_of_mins}')
 
 
 def main():
