a71db403a37ce1e33cab0430c33d9e4290df6b42
Jeff Blair <jeffreyblair@gmail.com>
2023-12-04 15:56:25 -0700

    Day3 part2 done: 5667240

--- 2023/day4.py
+++ 2023/day4.py
@@ -47,6 +47,28 @@ def part1(input_file):
 def part2(input_file):
     inputs = load_inputs(input_file)
 
+    cards = [1 for row in inputs]
+
+    for card in inputs:
+        card_id = int(card[5:card.index(':')])
+        winning_nums = [int(x) for x in str(card[card.index(':')+2:card.index('|')-1]).split(' ') if x != '']
+        my_nums = [int(x) for x in str(card[card.index('|')+2:]).split(' ') if x != '']
+
+        # print(f'{card_id}, {winning_nums}, {my_nums}')
+
+        matching_nums = 0
+        for num in my_nums:
+            if num in winning_nums:
+                matching_nums += 1
+
+        end_index = min(card_id+matching_nums, len(inputs))
+        for sub_index in range(card_id, end_index):
+            cards[sub_index] += 1 * cards[card_id-1]
+
+        print(f'cards: {cards}')
+
+    print(f'Total cards {sum(cards)}')
+
 
 def main():
     parser = argparse.ArgumentParser(description="Advent of Code.")
