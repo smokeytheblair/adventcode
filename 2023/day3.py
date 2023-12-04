f8d8da0d13211ac1bb07caa18326ed858706e0f4
Jeff Blair <jeffreyblair@gmail.com>
2023-12-03 19:31:19 -0700

    day3 part1 completed. needed a second sample file to figure it out.

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import numpy
 
 reset_report = None
 
@@ -19,17 +20,88 @@ def load_inputs(input_file):
     return report
 
 
-def isValidPos(x, y, n, m):
+def isValidPos(x, y, m, n):
     if x < 0 or y < 0 or x > n - 1 or y > m - 1:
-        return 0
-    return 1
+        return False
+    return True
+
+
+def is_symbol(inputs, x, y, m, n):
+    if x<0 or y<0:
+        return False
+    if x>=m or y>=n:
+        return False
+
+    if inputs[y][x] in ['!', '@', '#', '$', '%', '&', '*', '=', '+', '-', '/']:
+        return True
+
+
+def is_part_num(inputs, x, y, m, n):
+    if not inputs[y][x].isdigit():
+        return False
+
+    if is_symbol(inputs, x-1, y, m, n):
+        return True
+    if is_symbol(inputs, x+1, y, m, n):
+        return True
+    if is_symbol(inputs, x, y-1, m, n):
+        return True
+    if is_symbol(inputs, x, y+1, m, n):
+        return True
+    if is_symbol(inputs, x-1, y-1, m, n):
+        return True
+    if is_symbol(inputs, x-1, y+1, m, n):
+        return True
+    if is_symbol(inputs, x+1, y-1, m, n):
+        return True
+    if is_symbol(inputs, x+1, y+1, m, n):
+        return True
+
+    return False
+
+
+def get_part_num(inputs, x, y, m, n):
+    row = inputs[y].strip()
+
+    x_start = x
+    for new_x in range(x-1, -1, -1):
+        if str(row[new_x]).isdigit():
+            x_start = new_x
+        else:
+            break
+
+    x_end = x
+    for new_x in range(x+1,m):
+        if str(row[new_x]).isdigit():
+            x_end = new_x
+        else:
+            break
+
+    return int(row[x_start:x_end+1])
 
 
 def part1(input_file):
     inputs = load_inputs(input_file)
 
-    m = len(inputs)
-    n = len(inputs[0])
+    print(inputs)
+
+    n = len(inputs)
+    m = len(inputs[0].strip())
+
+    part_nums = []
+    for y in range(len(inputs)):
+        same_number = False
+        for x in range(len(inputs[y].strip())):
+            character = inputs[y][x]
+            if not character.isdigit():
+                same_number = False
+            if not same_number and is_part_num(inputs, int(x), int(y), m, n):
+                print(f'inputs[{x}, {y}]: part number {get_part_num(inputs, int(x), int(y), m, n)}')
+                part_nums.append(get_part_num(inputs, int(x), int(y), m, n))
+                same_number = True
+
+    print(part_nums)
+    print(sum(part_nums))
 
 
 def part2(input_file):
