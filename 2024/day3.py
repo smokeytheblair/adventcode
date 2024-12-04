18b3d3c0261f8c78c5dbf006c60182682cd0f3a3
Jeff Blair <jeffreyblair@gmail.com>
2024-12-03 20:40:10 -0700

    Day 3 pt 2 done. 103811193.

--- 2024/day3.py
+++ 2024/day3.py
@@ -40,7 +40,25 @@ def part1(input_file):
 
 def part2(input_file):
     inputs = load_inputs(input_file)
+    total_result = 0
 
+    do_dont = True
+    for input in inputs:
+        print(input)
+
+        muls = re.findall(r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)', input)
+        print(muls)
+
+        for mul in muls:
+            if mul == 'do()':
+                do_dont = True
+            elif mul == 'don\'t()':
+                do_dont = False
+            elif do_dont:
+                nums = re.findall(r'\d+', mul)
+                total_result += int(nums[0])*int(nums[1])
+
+    print(f"Total result: {total_result}")
 
 def main():
     parser = argparse.ArgumentParser(description="Advent of Code.")
