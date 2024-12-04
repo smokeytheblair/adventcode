df44e0380d5a5d05f4834cebd4680759dd819ed2
Jeff Blair <jeffreyblair@gmail.com>
2024-12-03 20:21:11 -0700

    Day 3 pt 1 done. 179571322.

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 from idlelib.configdialog import is_int
+import re
 
 reset_report = None
 
@@ -22,34 +23,17 @@ def load_inputs(input_file):
 
 def part1(input_file):
     inputs = load_inputs(input_file)
-
-    values = []
-
-    index = inputs[0].find("mul(")
-
-    while -1 < index:
-        # print(f"found at {index} - {inputs[0][index:index+10]}")
-        # find valid mul operator
-        comma = inputs[0].find(",", index+4)
-        close_paren = inputs[0].find(")", index+4)
-        val1 = inputs[0][index+4:comma]
-        val2 = inputs[0][comma+1:close_paren]
-
-        if is_int(val1) and is_int(val2):
-            values.append((int(val1), int(val2)))
-            print(f"ADDED val1: {val1}, val2: {val2}")
-        else:
-            print(f"SKIPPED val1:{val1}, val2: {val2}")
-
-        # remove current and step to the next
-        inputs[0] = inputs[0][index+4:]
-        index = inputs[0].find("mul(")
-
-    print(f"found values: {values}")
-
     total_result = 0
-    for mul in values:
-        total_result += mul[0] * mul[1]
+
+    for input in inputs:
+        print(input)
+
+        muls = re.findall(r'mul\(\d{1,3},\d{1,3}\)', input)
+        print(muls)
+
+        for mul in muls:
+            nums = re.findall(r'\d+', mul)
+            total_result += int(nums[0])*int(nums[1])
 
     print(f"Total result: {total_result}")
 
