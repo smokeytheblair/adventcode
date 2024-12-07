4519af7f99a709a104cb526240135ad862942622
Jeff Blair <jeffreyblair@gmail.com>
2024-12-07 14:49:14 -0700

    Day 6 pt 2 in progress...

--- 2024/day6.py
+++ 2024/day6.py
@@ -45,7 +45,7 @@ def walk_the_guard(map, pos_x, pos_y, current_direction, positions_count):
                     if (pos_x, pos_y) not in history:
                         positions_count += 1
                         history.append((pos_x, pos_y))
-            else: #done, off the map
+            else: # done, off the map
                 break
         elif current_direction == "east":
             if pos_x + 1 < len(map[pos_y]):
@@ -56,7 +56,7 @@ def walk_the_guard(map, pos_x, pos_y, current_direction, positions_count):
                     if (pos_x, pos_y) not in history:
                         positions_count += 1
                         history.append((pos_x, pos_y))
-            else: #done, off the map
+            else: # done, off the map
                 break
         elif current_direction == "west":
             if pos_x -1 >= 0:
@@ -69,7 +69,6 @@ def walk_the_guard(map, pos_x, pos_y, current_direction, positions_count):
                         history.append((pos_x, pos_y))
             else: # done, off the map
                 break
-
 
     return positions_count
 
@@ -108,8 +107,103 @@ def part1(input_file):
     positions_count = walk_the_guard(map, pos_x, pos_y, current_direction, positions_count)
     print(f"Unique positions visited: {positions_count}")
 
+
+def check_north(map, pos_x, pos_y):
+    ret = False
+
+    if pos_y > 0 and pos_x < len(map[0]):
+        for y in range(pos_y-1, 0):
+            if "#" == map[y][pos_x+1]:
+                if pos_x+2 < len(map[0]):
+                    for x in range(pos_x+2, len(map[0])):
+                        if "#" == map[y+1][x] and "." == map[pos_y+1][x-1]:
+                            ret = True
+                            print(f"check_north [{pos_x}, {pos_y}] = {ret} [{x-1}, {pos_y+1}")
+                            break
+                break
+
+    return ret
+
+
+def check_east(map, pos_x, pos_y):
+    ret = False
+
+    if pos_x < len(map[0]) and pos_y + 1 < len(map):
+        for x in range(pos_x+1, len(map[0])):
+            if "#" == map[pos_y+1][x]:
+                if pos_y+2 < len(map):
+                    for y in range(pos_y+2, len(map)):
+                        if "#" == map[y][x-1] and '.' == map[y-1][pos_x-1]:
+                            ret = True
+                            print(f"check_east [{pos_x}, {pos_y}] = {ret} [{pos_x-1}, {y-1}]")
+                            break
+                break
+
+    return ret
+
+
+def check_south(map, pos_x, pos_y):
+    print(f"check_south [{pos_x}, {pos_y}]")
+    ret = False
+
+    if pos_y > 0 and pos_x > 0:
+        for y in range(pos_y+1, len(map)):
+            if "#" == map[y][pos_x-1]:
+                if pos_x-2 > 0:
+                    for x in range(pos_x-2, 0):
+                        if "#" == map[y-1][x] and "." == map[pos_y-1][x+1]:
+                            ret = True
+                            print(f"check_north [{pos_x}, {pos_y}] = {ret} [{x+1}, {pos_y-1}")
+                            break
+                break
+
+    return ret
+
+
+def check_west(map, pos_x, pos_y):
+    print(f"check_west [{pos_x}, {pos_y}]")
+    ret = False
+
+    if pos_x > 0 and pos_y - 1 > 0:
+        for x in range(pos_x-1, 0):
+            if "#" == map[pos_y-1][x]:
+                if pos_y-2 < 0:
+                    for y in range(pos_y-2, 0):
+                        if "#" == map[y][x+1] and '.' == map[y+1][pos_x+1]:
+                            ret = True
+                            print(f"check_east [{pos_x}, {pos_y}] = {ret} [{pos_x+1}, {y+1}]")
+                            break
+                break
+
+    return ret
+
+
+def count_looping_positions(map):
+    print(f"count_looping_positions")
+    looping_positions = 0
+
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            if "#" == map[y][x]:
+                if check_north(map, x, y):
+                    looping_positions += 1
+                if check_east(map, x, y):
+                    looping_positions += 1
+                if check_west(map, x, y):
+                    looping_positions += 1
+                if check_south(map, x, y):
+                    looping_positions += 1
+
+    return looping_positions
+
+
 def part2(input_file):
     inputs = load_inputs(input_file)
+    map = [line.strip() for line in inputs]
+    print(f"map: {map}")
+
+    positions_count = count_looping_positions(map)
+    print(f"Looping positions: {positions_count}")
 
 
 def main():
