63fa92956d6318026d0165f8889093f6810398db
Jeff Blair <jeffreyblair@gmail.com>
2024-12-08 17:17:44 -0700

    Day 7 pt 2 complete (124060392153684)

--- 2024/day7.py
+++ 2024/day7.py
@@ -30,10 +30,8 @@ def next_permutation(operators):
     #     operators[least] = '+'
 
     countdown = list(range(len(operators)-1, -1, -1))
-    print(countdown)
-
     change_needed = True
-    for i in range(len(operators)-1, -1, -1):
+    for i in countdown:
         if operators[i] == '+' and change_needed:
             operators[i] = '*'
             change_needed = False
@@ -41,7 +39,34 @@ def next_permutation(operators):
             operators[i] = '+'
             change_needed = True
 
-    print(f"operators: {operators}")
+    # print(f"operators: {operators}")
+    return operators
+
+
+def next_permutation2(operators):
+    if '+' not in operators and '*' not in operators:
+        return None
+
+    # least = len(operators) - 1
+    # if operators[least] == '+':
+    #     operators[least] = '*'
+    # else:
+    #     operators[least] = '+'
+
+    countdown = list(range(len(operators)-1, -1, -1))
+    change_needed = True
+    for i in countdown:
+        if operators[i] == '+' and change_needed:
+            operators[i] = '*'
+            change_needed = False
+        elif operators[i] == '*' and change_needed:
+            operators[i] = '||'
+            change_needed = False
+        elif operators[i] == '||' and change_needed:
+            operators[i] = '+'
+            change_needed = True
+
+    # print(f"operators: {operators}")
     return operators
 
 
@@ -71,6 +96,34 @@ def can_be_true(result, operands):
     return 0
 
 
+def can_be_true2(result, operands):
+    operators = ['+' for i in range(len(operands)-1)]
+
+    while True:
+        ret = 0
+        temp_ops = operands.copy()
+        for i in range(len(operators)):
+            if '+' == operators[i]:
+                temp_ops[i+1] = temp_ops[i] + temp_ops[i+1]
+            elif '*' == operators[i]:
+                temp_ops[i+1] = temp_ops[i] * temp_ops[i+1]
+            elif '||' == operators[i]:
+                temp_ops[i+1] = int(str(temp_ops[i]) + str(temp_ops[i+1]))
+
+            ret = temp_ops[i+1]
+
+        if result == ret:
+            print(f"can_be_true({result}, {operands}) -> {result}")
+            return result
+        else:
+            operators = next_permutation2(operators)
+            if operators is None:
+                break
+
+    print(f"can_be_true({result}, {operands}) -> 0")
+    return 0
+
+
 def part1(input_file):
     inputs = load_inputs(input_file)
 
@@ -90,6 +143,17 @@ def part1(input_file):
 def part2(input_file):
     inputs = load_inputs(input_file)
 
+    total_sum = 0
+
+    for line in inputs:
+        result_index = line.find(":")
+        result = int(line[:result_index:])
+        operands = [int(x) for x in line[result_index+1::].strip().split(" ")]
+        print(f"result: {result}, operands: {operands}")
+
+        total_sum += can_be_true2(result, operands)
+
+    print(f"total_sum: {total_sum}")
 
 def main():
     parser = argparse.ArgumentParser(description="Advent of Code.")
